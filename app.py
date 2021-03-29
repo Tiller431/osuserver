cbd29d9ed9cb8dcbb59efad0c3e8bcbeb55f3479
Tiller431 <tylere431@gmail.com>
2021-03-29 04:24:56 +0000

    make some things async

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import discord
 import os
 from handlers import dockerHandler as docker
 import time
+import threading
 
 client = discord.Client()
 
@@ -19,23 +20,46 @@ async def on_message(message):
         return
 
     if content.startswith("-"):
-        await channel.send(f"----------\nProcessing command: {content}\nFrom: @{user}\n----------")
+        print(f"----------\nProcessing command: {content}\nFrom: @{user}\n----------")
 
     # Create a server
     if content.startswith("-osucreate"):
         start_time = round(time.time(), 2)
-        await channel.send("Starting creation process!")
-
-        url = docker.createServer()
-
-
-        await channel.send(f"Done! Took {round(time.time(), 2) - start_time} seconds!\nThe url to your server is: {url}")
+        process = threading.Thread(target=docker.createServer)
+        process.start()
+        await channel.send(f"The url to your server is: {docker.getURL()}\nPlease allow about 30 seconds for the server to start. ")
     
+    if content.startswith("-masscreate"):
+        message = "The urls to your servers are: \n"
+        try:
+            num_servers = int(content.split(" ")[1])
+        except:
+            await channel.send("please specify how many servers you would like")
+            return
+
+        if num_servers > 25:
+            await channel.send("damn bruh i dont have that much server power to give away")
+            return
+        for i in range(num_servers):
+            i = i + 1
+            process = threading.Thread(target=docker.createServer, args=(False, ))
+            process.start()
+            time.sleep(0.3)
+            message += f"https://osu.{i}.192.168.1.167.xip.io\n"
+        await channel.send(message)
+
+    if content.startswith("-getallservers"):
+        message = "These are all of the servers running: \n"
+        for i in range(docker.getNewID() - 1):
+            i = i + 1
+            message += f"https://osu.{i}.192.168.1.167.xip.io\n"
+        await channel.send(message)
+
     # Remove server
-    if content.startswith("-osurmall"):
+    if content.startswith("-rmall"):
         start_time = time.time()
         docker.rmALL()
-        await channel.send(f"Done! Took {start_time - time.time()} seconds!")
+        await channel.send("Done!")
 
     # Stop server
     if content.startswith("-osustop"):
@@ -49,7 +73,13 @@ async def on_message(message):
     if content.startswith("-restartnginx"):
         start_time = time.time()
         docker.rebuildNginx()
-        await channel.send(f"Done! Took {start_time - time.time()} seconds!")
+        await channel.send(f"Done! Took {time.time() - start_time} seconds!")
+    
+    # Restart all osu! servers
+    if content.startswith("-restartall"):
+        start_time = time.time()
+        docker.restartAll()
+        await channel.send(f"Done! Took {time.time() - start_time} seconds!")
 
 
 
--- handlers/dockerHandler.py
+++ handlers/dockerHandler.py
@@ -2,7 +2,9 @@ import os
 import requests
 import subprocess
 import fileinput
+import threading
 from handlers import nginxHandler as nginx
+import time
 
 def getNewID():
     return 1 + len(next(os.walk('servers'))[1])
@@ -28,7 +30,9 @@ def createFiles(user_id):
 def rmALL():
     for i in range(getNewID() - 1):
         i = i + 1
-        rmStack(i)
+        thread = threading.Thread(target=rmStack, args=(i, ))
+        thread.start()
+        time.sleep(0.1)
 
 def rmStack(user_id):
     stopCompose(user_id)
@@ -38,10 +42,8 @@ def rmStack(user_id):
     rmNginx(user_id)
 
 def updateNginx(user_id):
-    subprocess.run(["/usr/bin/docker-compose", "down", "--rmi", "all"], cwd=os.path.realpath(".") + "/nginx-backend")
     os.system(f"cp server_TEMPLATE.conf nginx-backend/servers/{user_id}.conf")
     subprocess.call(["sed", "-i", "-e",  f's/SERVER_ID/{user_id}/g', f"nginx-backend/servers/{user_id}.conf"])
-    subprocess.run(["/usr/bin/docker-compose", "up", "-d"], cwd=os.path.realpath(".") + "/nginx-backend")
 
 def rebuildNginx():
     subprocess.run(["/usr/bin/docker-compose", "kill"], cwd=os.path.realpath(".") + "/nginx-backend")
@@ -52,9 +54,22 @@ def rmNginx(user_id):
     rebuildNginx()
 
 
-def createServer():
+def createServer(autorestart=True):
     user_id = getNewID()
     createFiles(user_id)
     startCompose(user_id)
     updateNginx(user_id)
-    return f"https://osu.{user_id}.192.168.1.167.xip.io"
+    if autorestart == True:
+        rebuildNginx()
+
+def getURL():
+    return f"https://osu.{getNewID()}.192.168.1.167.xip.io"
+
+def restartAll():
+    for i in range(getNewID() - 1):
+        i = i + 1
+        restart(i)
+
+def restart(user_id):
+    subprocess.run(["/usr/bin/docker-compose", "down"], cwd=os.path.realpath(".") + "/servers/" + str(user_id))
+    subprocess.run(["/usr/bin/docker-compose", "up", "-d"], cwd=os.path.realpath(".") + "/servers/" + str(user_id))
